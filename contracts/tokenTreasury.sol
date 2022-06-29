ea08ac61a9fff43177ede1ba0828cee4f37174a4
Shantanu Bharadwaj <theshantanubharadwaj@gmail.com>
2022-06-29 10:42:46 +0530

    added tokenTreasury.sol

---
+++ contracts/tokenTreasury.sol
@@ -0,0 +1,22 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract tokenTreasury is Ownable {
+    uint256 public fundsAvailable;
+    address public sendTo;
+    bool public isReleased;
+
+    constructor(address _sendTo) payable {
+        fundsAvailable = msg.value;
+        sendTo = _sendTo;
+        isReleased = false;
+    }
+
+    function releaseFunds() public onlyOwner {
+        isReleased = true;
+        payable(sendTo).transfer(fundsAvailable);
+    }
+}
